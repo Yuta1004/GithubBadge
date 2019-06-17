a5d81ffc4a527b415264ab55940ff201cfe266a2
NakagamiYuta <guguru0014@gmail.com>
2019-06-17 23:47:48 +0900

    [add] indexページでバッジを表示するように

--- github_badge.py
+++ github_badge.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from shields import ShieldsColor, shields
 
 flask = Flask(__name__)
 
 
 @flask.route("/")
 def index():
-    return "Github Badge"
+    badge_url = shields("running", "OK", ShieldsColor.SUCCESS)
+    return "<image src=\"" + badge_url + "\"/image>"
 
 
 if __name__ == '__main__':
